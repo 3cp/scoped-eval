cf324b2ec5ab810cbd0d84e5ba7317591e761cd7
Chunpeng Huo <huocp@me.com>
2021-03-28 13:12:52 +1100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import modify from 'modify-code';
+import { traverse } from 'estraverse';
 import unusedName from './unused-name';
 import parse from './parse';
 import getGlobals from './get-globals';
@@ -22,6 +23,7 @@ const DEFAULT_ALLOWED_GLOBALS = {
   'Date': true,
   'Map': true,
   'Math': true,
+  'Object': true,
   'RegExp': true,
   'Set': true,
   'Selection': true,
@@ -39,11 +41,14 @@ const DEFAULT_ALLOWED_GLOBALS = {
   // 'clearImmediate': true,
 };
 
-export class ScopedEval {
+export default class ScopedEval {
   allowedGlobals: {[key: string]: boolean} = {...DEFAULT_ALLOWED_GLOBALS};
   cache: {[key: string]: (scope: any) => any} = {};
 
-  allowGlobals(globals: string[]) {
+  allowGlobals(globals: string | string[]) {
+    if (typeof globals === 'string') {
+      globals = [globals];
+    }
     if (globals) {
       for (const n of globals) {
         this.allowedGlobals[n] = true;
@@ -66,8 +71,8 @@ export class ScopedEval {
   preprocess(code: string): [string, string] {
     const ast = parse(code);
     const globals = getGlobals(ast, this.allowedGlobals);
-    console.log('globals', globals);
-    console.log('ast', JSON.stringify(ast, null, 2));
+    // console.log('globals', globals);
+    // console.log('ast', JSON.stringify(ast, null, 2));
     const scopeVariable = unusedName(globals);
 
     const m = modify(code);
@@ -81,12 +86,43 @@ export class ScopedEval {
       }
     }
 
-    // TODO rewrite foo to scope.get('foo')
-    // TODO rewrite foo += value to scope.set('foo', value, '+=')
+    // Rewrite foo += value to scope.set('foo', value, '+=')
+    traverse(ast, {
+      enter: function(node, parent) {
+        if (
+          parent &&
+          parent.type === 'AssignmentExpression' &&
+          parent.left === node &&
+          node.type === 'Identifier'
+        ) {
+          const {name, start, end} = node;
+          const ranges = globals[name];
+          // Not a global var
+          if (!ranges) return;
+          const found = ranges.findIndex(r => r.start === start && r.end === end);
+          // Still not a global var
+          if (found === -1) return;
+          // Remove from the global list
+          ranges.splice(found, 1);
+
+          const {right, operator} = parent;
+          m.replace(start, right.start, `${scopeVariable}.set('${name}', `);
+          m.insert(right.end, `, '${operator}')`);
+        }
+      }
+    });
+
     // TODO rewrite $parent.$parent.foo.bar to scope.get('$parent').get('$parent').get('foo').bar
 
-    // TODO support result.map for debugging
+    // Replace foo with scope.get('foo')
+    for (const name in globals) {
+      for (const {start, end} of globals[name]) {
+        m.replace(start, end, `${scopeVariable}.get('${name}')`);
+      }
+    }
+
     const result = m.transform();
+    // TODO support result.map for debugging
     return [scopeVariable, result.code];
   }
 }
